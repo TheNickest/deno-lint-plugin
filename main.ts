910d63e77e0d2ecab9113d6387ff72a7ce6ea2ba
TheNickest <coolnickde@yahoo.de>
2025-03-12 12:03:49 +0100

    docs: add module JSDoc and update version to 0.1.2 - Add module-level JSDoc documentation to main.ts - Update version from 1.0.0 to 0.1.2 following semver

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 /**
+ * A dummy module used to demonstrate the function-name-convention lint rule.
+ * @module
+ */
+
+/**
  * A dummy function used to demonstrate the function-name-convention lint rule.
  * This function intentionally violates the rule by starting with an uppercase letter.
  */
-
 function FooBar() {
   console.log('foo')
 }
